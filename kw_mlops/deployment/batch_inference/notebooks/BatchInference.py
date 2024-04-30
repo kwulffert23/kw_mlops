69ede135ce97c70f6f1a02fbc5de40a15cf4e509
Kyra Wulffert <kyra.wulffert@databricks.com>
2024-04-30 16:25:09 +0100

    Adding creation of inference table to batch-inference NB

--- kw_mlops/deployment/batch_inference/notebooks/BatchInference.py
+++ kw_mlops/deployment/batch_inference/notebooks/BatchInference.py
@@ -71,6 +71,16 @@ model_uri = f"models:/{model_name}@{alias}"
 
 # COMMAND ----------
 
+df = spark.table(
+    "delta.`dbfs:/databricks-datasets/nyctaxi-with-zipcodes/subsampled`"
+).drop("fare_amount")
+
+df.write.mode("overwrite").saveAsTable(
+    name="${bundle.target}.kw_mlops.feature_store_inference_input"
+)
+
+# COMMAND ----------
+
 from mlflow import MlflowClient
 
 # Get model version from alias
