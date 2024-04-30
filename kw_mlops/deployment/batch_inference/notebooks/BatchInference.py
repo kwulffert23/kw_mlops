526a989d4a0182978020535ba9326e50ad28d611
Kyra Wulffert <kyra.wulffert@databricks.com>
2024-04-30 17:01:18 +0100

    revert change in batch inference nb

--- kw_mlops/deployment/batch_inference/notebooks/BatchInference.py
+++ kw_mlops/deployment/batch_inference/notebooks/BatchInference.py
@@ -71,17 +71,6 @@ model_uri = f"models:/{model_name}@{alias}"
 
 # COMMAND ----------
 
-df = spark.table(
-    "delta.`dbfs:/databricks-datasets/nyctaxi-with-zipcodes/subsampled`"
-).drop("fare_amount")
-
-df.write.mode("overwrite").saveAsTable(
-    name="${bundle.target}.kw_mlops.feature_store_inference_input"
-)
-df.display()
-
-# COMMAND ----------
-
 from mlflow import MlflowClient
 
 # Get model version from alias
